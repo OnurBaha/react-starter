6c6b4aa137a658bd56b2740506971f4553ede1c7
OnurBaha <koconurbaha@gmail.com>
2024-01-10 17:04:16 +0300

    remove redux

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import {Link} from "react-router-dom";
 
 function Homepage() {
 
-	const cart = useSelector((state :any) => {
-		console.log(state);
-		return state.cart;
-	})
+
 
 	return (
 		<div>
 			Homepage
-			{cart.cartItems.length} sepette.
 			<br />
 			{/* İç linklerde href kullanımı YASAK */}
 			{/* <a href="/posts">Posts</a> */}
