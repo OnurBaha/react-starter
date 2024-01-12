d7c75536ee21c71c494e6714dbc2a27828981dce
OnurBaha <koconurbaha@gmail.com>
2024-01-12 17:21:18 +0300

    redux-thunk

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,22 +1,27 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "semantic-ui-react";
 import {cartActions} from "../../store/slices/cartSlice";
+import { Post } from "../../models/post";
+import PostCard from "../../components/PostCard/PostCard";
+import { getAllPosts } from "../../store/slices/postSlice";
 function Posts() {
 
 	const dispatch = useDispatch();
+
+	const postState = useSelector((state: any) => state.post);
+	console.log(postState);
+	useEffect(() => {
+		dispatch(getAllPosts() as any);
+	}, []);
 
 	return (
 		<div>
 			Posts
 
-			<Button
-				onClick={() => {
-					dispatch(cartActions.addToCart({id: 1, name: "Deneme", price: 50}));
-				}}
-			>
-				Sepete Ekle
-			</Button>
+			{postState.posts.map((post: Post) => (
+				<PostCard post={post} />
+			))}
 		</div>
 	);
 }
--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import postServices from "../../services/postServices";
 
 
 export const cartSlice = createSlice({
